c72736ed4070362fd40473f2fb11cf3714242fb5
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-05-15 13:09:06 -0300

    fixed error void return on middleware authentication

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -6,7 +6,10 @@ const SECRET = 'mySecret';
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader?.split(' ')[1];
-  if (!token) return res.sendStatus(401);
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
 
   jwt.verify(token, SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
