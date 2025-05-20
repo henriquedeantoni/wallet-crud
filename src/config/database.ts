601ed8f169a8e90e559ac5aeee7550ea9a49146c
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-05-19 22:23:01 -0300

    rewrite database

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,8 +4,8 @@ import mysql from 'mysql2/promise';
 dotenv.config();  
 
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'passwordTemporary',
-  database: 'investment_wallet',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
