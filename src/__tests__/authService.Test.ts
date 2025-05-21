7ffe1380d9001002cb3cc051aa61d057a655e123
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-05-20 22:16:52 -0300

    completed authservice

--- src/__tests__/authService.Test.ts
+++ src/__tests__/authService.Test.ts
@@ -19,3 +19,73 @@ jest.mock('jsonwebtoken', ()=>({
     sign: jest.fn(() => 'fake-jwt-token'),
     verify: jest.fn(() => ({ id: 1, email: 'exampletesting@email.com'}))
 }));
+
+describe('AuthService', () =>{
+    beforeEach(()=>{
+        jest.clearAllMocks();
+    })
+
+    it('registrar um novo usuário', async()=>{
+        const userData = {
+            email: 'exampleTesting@email.com',
+            firstName: 'John',
+            lastName: 'Doe',
+            tel: '219912031254',
+            password: 'fictionalpassword123456'
+        };
+
+        await authService.register(userData);
+
+        expect(mockUserRepository.create).toHaveBeenCalledWith(
+            expect.objectContaining({
+            email: userData.email,
+            password: expect.any(String)
+            })
+        )
+    });
+
+    it('faz login com sucesso e retorna um token', async () => {
+        const originalPassword = '1234pass';
+        const hashedPassword = await bcrypt.hash(originalPassword, 10);
+
+        const mockUser: User = {
+            id: 1,
+            email: 'exampleTesting@email.com',
+            firstName: 'John',
+            lastName: 'Doe',
+            tel: '219912031254',
+            password: hashedPassword,
+            createdAt: new Date(),
+            updatedAt: new Date()
+
+        };
+
+        mockUserRepository.findByEmail.mockResolvedValue(mockUser);
+
+        const token = await authService.login(mockUser.email, originalPassword);
+        
+        expect(mockUserRepository.findByEmail).toHaveBeenCalledWith(mockUser.email);
+        expect(token).toBe('fake-jwt-token');
+    });
+
+    it('Lança erro se a senha for inválida', async () => {
+    
+        const hashedPassword = await bcrypt.hash('rightPass', 10);
+        const mockUser = { ...mockUserRepository, password: hashedPassword };
+
+        mockUserRepository.findByEmail.mockResolvedValue(mockUser);
+
+        await expect(authService.login('exampleTesting@email.com', 'wrongPass'))
+        .rejects
+        .toThrow('Invalid credentials');
+    });
+
+    it('Lança erro se o usuário não existir', async () => {
+        mockUserRepository.findByEmail.mockResolvedValue(null);
+
+        await expect(authService.login('notfound@example.com', '1234'))
+        .rejects
+        .toThrow('User not found');
+    });
+
+})
